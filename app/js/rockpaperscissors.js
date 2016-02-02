cf6ca0eb84d9c07186649206c81c54beeb118fc3
mgundies <marc.g.underwood@gmail.com>
2016-02-02 22:46:33 +1300

    updated getPlayerMove

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,11 +22,17 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
+	if ((move = null)||(move = undefined) {
+        getPlayerMove(getInput);
+    } else {
+        return move;
+    };
+};
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
-}
+    // return /* Your Expression */;
+
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
